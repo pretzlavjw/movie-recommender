77b90380827872f014ae30002ee5260a2f88aa8c
Brandon Long <brandon6348@gmail.com>
2021-08-05 16:18:53 -0400

    added exceptions package and UserNotFoundException

---
+++ java/src/main/java/com/techelevator/exceptions/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.techelevator.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found.")
+public class UserNotFoundException extends Exception{
+    private static final long serialVersionUID = 1L;
+    public UserNotFoundException() {
+        super("User not found.");
+    }
+}
