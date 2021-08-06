6747928f933a8c1b96c102d48ba42af0a708793a
Brandon Long <brandon6348@gmail.com>
2021-08-06 15:25:50 -0400

    added functionality to MovieController to get movie and watchlist

--- java/src/main/java/com/techelevator/controller/MovieController.java
+++ java/src/main/java/com/techelevator/controller/MovieController.java
@@ -1,5 +1,6 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.MovieDAO;
 import com.techelevator.exceptions.MovieNotFoundException;
 import com.techelevator.model.Movie;
 import com.techelevator.services.RESTMovieService;
@@ -7,12 +8,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/movie")
 @PreAuthorize("isAuthenticated()")
 public class MovieController {
 
     private RESTMovieService restMovieService;
+    private MovieDAO movieDAO;
+
+    public MovieController (MovieDAO movieDAO) {
+        this.movieDAO = movieDAO;
+    }
 
     @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
@@ -20,6 +28,22 @@ public class MovieController {
     public void addNewMovie(@PathVariable String imdbId) throws MovieNotFoundException {
         Movie newMovie = restMovieService.getMovie(imdbId);
 
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path = "/watchlist/{userId}", method = RequestMethod.GET)
+    public List<Movie> getUserMovieWatchList(@PathVariable Long userId) throws MovieNotFoundException {
+        List<Movie> watchList = movieDAO.getUserMovieWatchList(userId);
+        if(watchList == null) throw new MovieNotFoundException();
+        else return watchList;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path = "/get-movie/{userId}", method = RequestMethod.GET)
+    public Movie getRecommendedMovie(@PathVariable Long userId) throws MovieNotFoundException {
+        Movie recommendedMovie = movieDAO.getMovie(userId);
+        if(recommendedMovie == null) throw new MovieNotFoundException();
+        else return recommendedMovie;
     }
 
 }
