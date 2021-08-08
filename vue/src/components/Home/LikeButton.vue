c3a6a58382f058973dc52722ef250e78130a6287
Alexander Sonnie <Sonnie.2@live.com>
2021-08-08 16:52:28 -0400

    Made Like and Dislike buttons for app

---
+++ vue/src/components/Home/LikeButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="like-container">
+    <button id="like" class="btn btn-primary" v-on:click="LikeMovie">Like</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LikeButton",
+  methods: {
+    LikeMovie() {
+      this.$emit("Like");
+    }
+  }
+};
+</script>
+
+<style>
+div.like-container {
+  display: inline-block;
+}
+</style>
