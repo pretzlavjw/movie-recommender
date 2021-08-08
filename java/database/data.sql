1265836beb4edfe61ac8d7e44f358d00b390cd0a
John Pretzlav <jpretz27@gmail.com>
2021-08-08 19:54:54 -0400

    Populated database with movies and genre IDs

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,1 +1,44 @@
+INSERT INTO genre (genre_name) VALUES ('Action'), ('Horror'), ('Comedy'), ('Mystery'), ('Drama'), ('Romance'), ('Fantasy'), ('Thriller'), ('Western'), ('Sci-Fi');
 
+
+INSERT INTO movies (imdb_id, movie_genre, movie_title, movie_description, movie_image, year_released, rating, movie_length)
+VALUES ('tt0093779', 'Adventure, Family, Fantasy', 'The Princess Bride', 'While home sick in bed, a young boy''s grandfather reads him the story of a farmboy-turned-pirate who encounters numerous obstacles, enemies and allies in his quest to be reunited with his true love.', 'https://m.media-amazon.com/images/M/MV5BMGM4M2Q5N2MtNThkZS00NTc1LTk1NTItNWEyZjJjNDRmNDk5XkEyXkFqcGdeQXVyMjA0MDQ0Mjc@._V1_SX300.jpg', '1987', 'PG', '98 min'
+        ),
+        ('tt0088930', 'Comedy, Crime, Mystery', 'Clue', 'Six guests are anonymously invited to a strange mansion for dinner, but after their host is killed, they must cooperate with the staff to identify the murderer as the bodies pile up.', 'https://m.media-amazon.com/images/M/MV5BM2VlNTE1ZmMtOTAyNS00ODYwLWFmY2MtZWEzOTE2YjE1NDE2XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg', '1985', 'PG', '94 min'
+        ),
+        ('tt0848228','Action, Adventure, Sci-Fi','The Avengers', 'Earth''s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.', 'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg', '2012', 'PG-13', '143 min'
+        ),
+        ('tt0133093', 'Action, Sci-Fi', 'The Matrix', 'When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.', 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg', '1999', 'R', '136 min'
+        ),
+        ('tt1156398', 'Adventure, Comedy, Fantasy', 'Zombieland', 'A shy student trying to reach his family in Ohio, a gun-toting bruiser in search of the last Twinkie and a pair of sisters striving to get to an amusement park join forces in a trek across a zombie-filled America', 'https://m.media-amazon.com/images/M/MV5BMTU5MDg0NTQ1N15BMl5BanBnXkFtZTcwMjA4Mjg3Mg@@._V1_SX300.jpg', '2009', 'R', '88 min'
+        ),
+        ('tt0111161', 'Drama', 'The Shawshank Redemption', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg', '1994', 'R', '142 min'
+        ),
+        ('tt0034583', 'Drama, Romance, War', 'Casablanca', 'A cynical expatriate American cafe owner struggles to decide whether or not to help his former lover and her fugitive husband escape the Nazis in French Morocco.', 'https://m.media-amazon.com/images/M/MV5BY2IzZGY2YmEtYzljNS00NTM5LTgwMzUtMzM1NjQ4NGI0OTk0XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg', '1942', 'PG', '102 min'
+        ),
+       -- ('tt0071230', 'Comedy, Western', 'Blazing Saddles', 'In order to ruin a western town, a corrupt politician appoints a black Sheriff, who promptly becomes his most formidable adversary.', '1974', 'R', '93 min'
+        --),
+        ('tt0076759', 'Action, Adventure, Fantasy', 'Star Wars', 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire''s world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vad', 'https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg', '1977', 'PG', '121 min'
+        ),
+        ('tt0062622','Adventure, Sci-Fi','2001: A Space Odyssey', 'After discovering a mysterious artifact buried beneath the Lunar surface, mankind sets off on a quest to find its origins with help from intelligent supercomputer H.A.L. 9000.', 'https://m.media-amazon.com/images/M/MV5BMmNlYzRiNDctZWNhMi00MzI4LThkZTctMTUzMmZkMmFmNThmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg', '1970', 'G', '149 min'
+        ),
+        ('tt0116483', 'Comedy, Sport', 'Happy Gilmore', 'A rejected hockey player puts his skills to the golf course to save his grandmother''s house.', 'https://m.media-amazon.com/images/M/MV5BZWI2NjliOTYtZjE1OS00YzAyLWJjYTQtYWNmZTQzMTQzNzVjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg', '1996', 'PG-13', '92 min'
+        ),
+        ('tt0078748', 'Horror, Sci-Fi', 'Alien', 'After a space merchant vessel receives an unknown transmission as a distress call, one of the crew is attacked by a mysterious life form and they soon realize that its life cycle has merely begun.', 'https://m.media-amazon.com/images/M/MV5BMmQ2MmU3NzktZjAxOC00ZDZhLTk4YzEtMDMyMzcxY2IwMDAyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg', '1979', 'R', '117 min'
+        ),
+        ('tt1591095', 'Horror, Mystery, Thriller', 'Insidious', 'A family looks to prevent evil spirits from trapping their comatose child in a realm called The Further.', 'https://m.media-amazon.com/images/M/MV5BMTYyOTAxMDA0OF5BMl5BanBnXkFtZTcwNzgwNTc1NA@@._V1_SX300.jpg', '2010', 'PG-13', '103 min'
+        ),
+        ('tt0441773', 'Animation, Action, Adventure', 'Kung Fu Panda', 'The Dragon Warrior has to clash against the savage Tai Lung as China''s fate hangs in the balance. However, the Dragon Warrior mantle is supposedly mistaken to be bestowed upon an obese panda who is a novice in martial arts.', 'https://m.media-amazon.com/images/M/MV5BODJkZTZhMWItMDI3Yy00ZWZlLTk4NjQtOTI1ZjU5NjBjZTVjXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg', '2008', 'PG', '92 min'
+        ),
+        ('tt0120737', 'Action, Adventure, Drama', 'The Lord of the Rings: The Fellowship of the Ring', 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.', 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg', '2001', 'PG-13', '178 min'
+        ),
+        ('tt1825683', 'Action, Adventure, Sci-Fi', 'Black Panther', 'T''Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country''s past.', 'https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_SX300.jpg', '2018', 'PG-13', '134 min'
+        ),
+        ('tt0371746', 'Action, Adventure, Sci-Fi', 'Iron Man', 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.', 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg', '2008', 'PG-13', '126 min'
+        ),
+        ('tt2494362', 'Drama, Horror, Western', 'Bone Tomahawk', 'In the dying days of the old west, an elderly sheriff and his posse set out to rescue their town''s doctor from cannibalistic cave dwellers.', 'https://m.media-amazon.com/images/M/MV5BMzQ0MzE4OTUzMF5BMl5BanBnXkFtZTgwODAyNzI3NjE@._V1_SX300.jpg', '2016', 'Not Rated', '132 min'
+        ),
+        ('tt1403865', 'Drama, Western', 'True Grit', 'A stubborn teenager enlists the help of a tough U.S. Marshal to track down her father''s murderer.', 'https://m.media-amazon.com/images/M/MV5BODhkZDIzNjgtOTA5ZS00MmMzLWFkNjYtM2Y2MzFjN2FkNjAzL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg', '2010', 'PG-13', '110 min'
+        );
+        
+        
