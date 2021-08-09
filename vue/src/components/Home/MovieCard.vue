7be44d509f7199b07890da3cb985d542a9af1d9e
Alexander Sonnie <Sonnie.2@live.com>
2021-08-09 17:26:16 -0400

    Able to load imgs and descriptions of movies still need to use api to fill all of the respective fields in

--- vue/src/components/Home/MovieCard.vue
+++ vue/src/components/Home/MovieCard.vue
@@ -2,8 +2,9 @@
 
   <div class="movie-card">
       <h3 class="movie-title">Movie Title</h3>
-      <img v-bind:src="url">
-      <h4 class="movie-plot">Description</h4>
+      <img src="https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg">
+      <h4 class="movie-plot">A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron</h4>
+      
       <div class="movie-info">
         <p>genre</p>
         <p>length</p>
